1f63c8d8617a40d8c3c299bef985317100eb79dd
Rene Ventura <venturar729@gmail.com>
2018-12-18 22:17:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,10 @@ def line(deli_line)
   line_description = "The line is currently"
   if deli_line.size == 0
     line_description += " empty."
-    puts line_description
   else
     deli_line.each_with_index do |person, place|
       line_description += ": #{place + 1}. #{person}"
     end
   end
-  line_description
+  puts line_description
 end
