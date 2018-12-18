1674c27432909fc79a58146681759dfc804de580
Rene Ventura <venturar729@gmail.com>
2018-12-18 22:16:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(deli_line)
   line_description = "The line is currently"
   if deli_line.size == 0
     line_description += " empty."
+    puts line_description
   else
     deli_line.each_with_index do |person, place|
       line_description += ": #{place + 1}: #{person}"
